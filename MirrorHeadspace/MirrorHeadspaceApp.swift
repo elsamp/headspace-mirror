eb7e3212fb6db9bd6ca637d51a27b048bd30678e
Erica Sampson <ericabcherry@gmail.com>
2024-07-29 09:51:58 -0400

    Initial Commit

---
+++ MirrorHeadspace/MirrorHeadspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MirrorHeadspaceApp.swift
+//  MirrorHeadspace
+//
+//  Created by Erica Sampson on 2024-07-29.
+//
+
+import SwiftUI
+
+@main
+struct MirrorHeadspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
